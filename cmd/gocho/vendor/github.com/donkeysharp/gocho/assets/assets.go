1549d4324318246b2dd1156b6fdd484b57c3c883
Artem <temorfeouz@gmail.com>
2020-05-24 23:35:38 +0300

    remove trash

--- cmd/gocho/vendor/github.com/donkeysharp/gocho/assets/assets.go
+++
@@ -1,9 +0,0 @@
-package assets
-
-import (
-	"github.com/elazarl/go-bindata-assetfs"
-)
-
-func AssetFS() *assetfs.AssetFS {
-	return &assetfs.AssetFS{Asset: Asset, AssetDir: AssetDir, AssetInfo: AssetInfo, Prefix: "../../ui/build"}
-}
--- cmd/gocho/vendor/github.com/donkeysharp/gocho/pkg/info/info.go
+++
@@ -1,6 +0,0 @@
-package info
-
-const (
-	APP_NAME = "gocho"
-	VERSION  = "0.1.0-alfa"
-)
